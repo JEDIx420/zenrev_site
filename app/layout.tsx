2a587fb937fe2bc9c9036beeadb0a2a645991702
Vincy Vincent <vincyvincent@Vincys-MacBook-Air.local>
2025-12-16 17:57:19 +0530

    Add Factors SDK to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter, Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/layout/Header";
@@ -89,6 +90,11 @@ export default function RootLayout({
         <main className="relative z-10 flex-1">{children}</main>
         <Footer />
         <Chatbot />
+        <Script id="factors-sdk" strategy="beforeInteractive">
+          {`
+            window.faitracker=window.faitracker||function(){this.q=[];var t=new CustomEvent("FAITRACKER_QUEUED_EVENT");return this.init=function(t,e,a){this.TOKEN=t,this.INIT_PARAMS=e,this.INIT_CALLBACK=a,window.dispatchEvent(new CustomEvent("FAITRACKER_INIT_EVENT"))},this.call=function(){var e={k:"",a:[]};if(arguments&&arguments.length>=1){for(var a=1;a<arguments.length;a++)e.a.push(arguments[a]);e.k=arguments[0]}this.q.push(e),window.dispatchEvent(t)},this.message=function(){window.addEventListener("message",function(t){"faitracker"===t.data.origin&&this.call("message",t.data.type,t.data.message)})},this.message(),this.init("z7rzvvnjeaig2uhmoiiispfy2ybq3lwm",{host:"https://api.factors.ai"}),this}(),function(){var t=document.createElement("script");t.type="text/javascript",t.src="https://app.factors.ai/assets/factors.js",t.async=!0,(d=document.getElementsByTagName("script")[0]).parentNode.insertBefore(t,d)}();
+          `}
+        </Script>
       </body>
     </html>
   );
